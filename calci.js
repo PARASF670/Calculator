2210ea9f2c7c7a8a3cccfd696e3297483365d141
PARASF670 <42441544+PARASF670@users.noreply.github.com>
2021-05-16 00:10:28 +0530

    Update calci.js

--- calci.js
+++ calci.js
@@ -15,7 +15,7 @@ card_header.style.backgroundColor = 'rgb(221,220,220)';
 var output_label = document.createElement('label');
 output_label.id = "output";
 output_label.style.minWidth = "20px";
-output_label.style.maxWidth = "50px";
+output_label.style.maxWidth = "200px";
 output_label.innerText = "";
 var input = document.createElement('input');
 input.setAttribute("type", "text");
